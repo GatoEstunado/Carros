9b6ae248dbbd47a2433ac36294a6085ccca241a6
Dennis <126710731+GatoEstunado@users.noreply.github.com>
2023-04-18 14:48:19 -0300

    Update

--- Carro.cpp
+++ Carro.cpp
@@ -1,7 +1,10 @@
+// Bibliotecas
 #include <iostream>
 using namespace std;
 
+// Criar a "Classe", Classe Carro
 class Carro {
+    // Variáveis, "públicas"
     public:
     string color;
     string modelo;
@@ -9,19 +12,24 @@ class Carro {
     void buzinar();
 };
 
+// Programa pricipal
 int main() {
+    // Criar uma subclasse através da "Classe Carro", MeuCarro
     Carro MeuCarro;
     
+    // As variáveis da subclasse
     MeuCarro.color = "Preto";
     MeuCarro.modelo = "Opala";
     MeuCarro.VelMax = 200.0f;
     
+    // Interface do usuário
     MeuCarro.buzinar();
     cout << "Seu novo carro é um: " << MeuCarro.modelo << endl;    
     cout << "A cor dele é: " << MeuCarro.color << endl;
     cout << "Ele chega a: " << MeuCarro.VelMax << " km/h" << endl;
 }
 
+// Método
 void Carro::buzinar() {
     cout << "Bi, Biii!" << endl;
 }
