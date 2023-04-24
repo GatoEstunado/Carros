12b5bedf8d31a0c4c7b9a42f104dab798241c502
Dennis <126710731+GatoEstunado@users.noreply.github.com>
2023-04-24 20:16:52 -0300

    Update

---
+++ Construtor Carro.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Carro {
+    public:
+    
+    string Cor;
+    string Modelo;
+    float VelMax;
+    
+    void Buzinar();
+    
+    Carro() {
+        Cor = "Amarelo";
+        Modelo = "Camaro";
+        VelMax = 220.f;
+    }
+};
+
+int main() {
+    Carro MeuCarro;
+    Carro Carro2;
+    
+    MeuCarro.Cor = "Branco";
+    MeuCarro.Modelo = "Fusca";
+    MeuCarro.VelMax = 150.f;
+    
+    MeuCarro.Buzinar();
+    cout << MeuCarro.Cor << endl;
+    cout << MeuCarro.Modelo << endl;
+    cout << MeuCarro.VelMax << endl;
+    
+    Carro2.Buzinar();
+    cout << Carro2.Cor << endl;
+    cout << Carro2.Modelo << endl;
+    cout << Carro2.VelMax << endl;
+}
